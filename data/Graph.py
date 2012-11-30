0251963276e922391beb80587ea5ceecdd2f6ba8
Ilya Biryukov <iu.biryukov@gmail.com>
2012-11-30 21:32:55 +0400

    A prototype of graph

---
+++ data/Graph.py
@@ -0,0 +1,18 @@
+class Graph(object):
+    def __init__(self, vertices_count):
+        # Initialize
+        self.__vertices = [list() for n in vertices_count]
+        pass
+
+
+    def get_vertices_count(self):
+        return len(self.__vertices)
+
+
+    def adjacent_vertices(self, vertex_index):
+        return self.__vertices[vertex_index]
+
+
+    def add_edge(self, from_vertex_id, to_vertex_id):
+        self.__vertices[from_vertex_id].append(to_vertex_id)
+        self.__vertices[to_vertex_id].append(from_vertex_id)
