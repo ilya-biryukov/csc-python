b32576931e32911d34f53e21a1dee9e3ff2a7f64
Nikita.Tolstikov <iammmann@gmail.com>
2012-12-01 20:46:35 +0300

    some tries

--- algo/graph_builder/__init__.py
+++ algo/graph_builder/__init__.py
@@ -3,9 +3,10 @@ import shapefile
 
 reader = shapefile.Reader('WORLD_MAP/WORLD_MAP')
 sr = reader.shapeRecords()
-print sr[0].record[4]
-for part in sr[0].shape.parts:
-    print part
+for i in xrange(len(sr)):
+    print sr[i].record[4]
+    for part in sr[i].shape.parts:
+        print part
 
-for point in sr[0].shape.points:
-    print point
+#for point in sr[0].shape.points:
+#    print point
