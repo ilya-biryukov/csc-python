c24f4b611bda0447412f22f174b5d379f8770734
Nikita.Tolstikov <iammmann@gmail.com>
2012-12-01 18:11:45 +0300

    Changes to polygon and graph
    Polygon - create point array
    Graph - named vertex

--- data/Graph.py
+++ data/Graph.py
@@ -2,8 +2,11 @@ class Graph(object):
     def __init__(self, vertices_count):
         # Initialize
         self.__vertices = [list() for n in vertices_count]
+        self.__vertices_names = [list() for n in vertices_count]
         pass
 
+    def add_vertex_name(self, vertex_id, vertex_name):
+        self.__vertices_names[vertex_id] = vertex_name
 
     def get_vertices_count(self):
         return len(self.__vertices)
@@ -16,3 +19,4 @@ class Graph(object):
     def add_edge(self, from_vertex_id, to_vertex_id):
         self.__vertices[from_vertex_id].append(to_vertex_id)
         self.__vertices[to_vertex_id].append(from_vertex_id)
+
--- data/Polygon.py
+++ data/Polygon.py
@@ -1,14 +1,16 @@
+from Point import Point
+
 class Polygon(object):
     def __init__(self, points):
         """ Initialize polygon with a list of (float, float) pairs --- points of its' vertices
         """
-        self.__points = points
+        self.__points = []
+        for p in points:
+            self.__points.append(p)
         pass
-
 
     def get_points(self):
         return self.__points
-
 
     points = property(get_points)
 
